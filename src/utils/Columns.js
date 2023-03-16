2cae60de24d4fc78296194dc2059c93ac542addb
kusham <kusham1998@gmail.com>
2023-03-16 22:56:12 +0530

    column add

---
+++ src/utils/Columns.js
@@ -0,0 +1,18 @@
+export const columns = [
+    {
+      title: "SNo",
+      dataIndex: "key",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Product",
+      dataIndex: "product",
+    },
+    {
+      title: "Status",
+      dataIndex: "staus",
+    },
+  ];
