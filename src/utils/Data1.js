89309272a1ad14687f355bd7858b11fb6cc0c6e1
kusham <kusham1998@gmail.com>
2023-03-16 22:55:51 +0530

    data1 add

---
+++ src/utils/Data1.js
@@ -0,0 +1,9 @@
+export const data1 = [];
+for (let i = 0; i < 46; i++) {
+  data1.push({
+    key: i,
+    name: `Edward King ${i}`,
+    product: 32,
+    staus: `London, Park Lane no. ${i}`,
+  });
+}
