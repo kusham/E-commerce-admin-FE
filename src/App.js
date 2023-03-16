ffe2848e5cb4313f5ae5e084f3e8f95df3f3d254
kusham <kusham1998@gmail.com>
2023-03-16 22:47:04 +0530

    add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+  return <Router></Router>;
 }
 
 export default App;
