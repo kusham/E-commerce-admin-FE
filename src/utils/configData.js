a2f8566ab0922acbc527a9f6ab125942c1903588
kusham <kusham1998@gmail.com>
2023-03-16 22:56:04 +0530

    config data add

---
+++ src/utils/configData.js
@@ -0,0 +1,29 @@
+export const config = {
+    data,
+    xField: "type",
+    yField: "sales",
+    color: ({ type }) => {
+      return "#ffd333";
+    },
+    label: {
+      position: "middle",
+      style: {
+        fill: "#FFFFFF",
+        opacity: 1,
+      },
+    },
+    xAxis: {
+      label: {
+        autoHide: true,
+        autoRotate: false,
+      },
+    },
+    meta: {
+      type: {
+        alias: "Month",
+      },
+      sales: {
+        alias: "Income",
+      },
+    },
+  };
