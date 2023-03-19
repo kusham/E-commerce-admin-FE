6ac829ba18a02155005590c41dbe23b2c5fe101d
kusham <kusham1998@gmail.com>
2023-03-19 23:09:04 +0530

    order table column data2

---
+++ src/utils/OrderColumns2.js
@@ -0,0 +1,27 @@
+export const columns = [
+    {
+      title: "SNo",
+      dataIndex: "key",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Product",
+      dataIndex: "product",
+    },
+    {
+      title: "Amount",
+      dataIndex: "amount",
+    },
+    {
+      title: "Date",
+      dataIndex: "date",
+    },
+  
+    {
+      title: "Action",
+      dataIndex: "action",
+    },
+  ];
