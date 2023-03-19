7e0522df2802a6d44242b5c9130f9c626fa88e2a
kusham <kusham1998@gmail.com>
2023-03-19 23:08:57 +0530

    order table column data

---
+++ src/utils/OrderColumns.js
@@ -0,0 +1,35 @@
+export const columns = [
+    {
+      title: "SNo",
+      dataIndex: "key",
+    },
+    {
+      title: "Product Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Brand",
+      dataIndex: "brand",
+    },
+    {
+      title: "Count",
+      dataIndex: "count",
+    },
+    {
+      title: "Color",
+      dataIndex: "color",
+    },
+    {
+      title: "Amount",
+      dataIndex: "amount",
+    },
+    {
+      title: "Date",
+      dataIndex: "date",
+    },
+  
+    {
+      title: "Action",
+      dataIndex: "action",
+    },
+  ];
