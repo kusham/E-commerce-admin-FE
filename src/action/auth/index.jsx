f7c7750eab95cc063ef24d45e30f5e711fe74ee3
kusham <kusham1998@gmail.com>
2023-03-20 22:09:44 +0530

    getOrderByUser api

--- src/action/auth/index.jsx
+++ src/action/auth/index.jsx
@@ -1,11 +1,22 @@
 import {createAsyncThunk } from "@reduxjs/toolkit";
-import {login} from '../../api/auth'
+import {getOrder, login} from '../../api/auth'
 
 export const login = createAsyncThunk(
     "auth/login",
     async (userData, thunkAPI) => {
       try {
         return await login(userData);
+      } catch (error) {
+        return thunkAPI.rejectWithValue(error);
+      }
+    }
+  );
+
+  export const getOrderByUser = createAsyncThunk(
+    "order/get-order",
+    async (id, thunkAPI) => {
+      try {
+        return await getOrder(id);
       } catch (error) {
         return thunkAPI.rejectWithValue(error);
       }
