a5fa423ba462fbac5504ee013ea4d31a893366e7
kusham <kusham1998@gmail.com>
2023-03-21 22:11:00 +0530

    getAOrder action

--- src/api/auth.jsx
+++ src/api/auth.jsx
@@ -1,11 +1,19 @@
 import axios from "axios";
 import { base_url } from "../utils/baseUrl";
 
-
 export const login = async (user) => {
   const response = await axios.post(`${base_url}user/admin-login`, user);
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data));
   }
+  return response.data;
+};
+
+export const getOrder = async (id) => {
+  const response = await axios.post(
+    `${base_url}user/getorderbyuser/${id}`,
+    "",
+    config
+  );
   return response.data;
 };
