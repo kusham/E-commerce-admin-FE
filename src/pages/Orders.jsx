e934428ac588534bd627acc02817508503e2f3b8
kusham <kusham1998@gmail.com>
2023-03-25 23:18:43 +0530

    render order details in the table

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,14 +1,48 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import { Table } from "antd";
-import { columns } from '../utils/OrderColumns2';
+import { columns } from "../utils/OrderColumns2";
+import { getOrders } from "../action/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { BiEdit } from "react-icons/bi";
+import { AiFillDelete } from "react-icons/ai";
 
 const Orders = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getOrders());
+  }, []);
+  const orderState = useSelector((state) => state.auth.orders);
+
+  const data1 = [];
+  for (let i = 0; i < orderState.length; i++) {
+    data1.push({
+      key: i + 1,
+      name: orderState[i].orderby.firstname,
+      product: (
+        <Link to={`/admin/order/${orderState[i].orderby._id}`}>
+          View Orders
+        </Link>
+      ),
+      amount: orderState[i].paymentIntent.amount,
+      date: new Date(orderState[i].createdAt).toLocaleString(),
+      action: (
+        <>
+          <Link to="/" className=" fs-3 text-danger">
+            <BiEdit />
+          </Link>
+          <Link className="ms-3 fs-3 text-danger" to="/">
+            <AiFillDelete />
+          </Link>
+        </>
+      ),
+    });
+  }
   return (
     <div>
       <h3 className="mb-4 title">Orders</h3>
-      <div>{<Table columns={columns} dataSource={} />}</div>
+      <div>{<Table columns={columns} dataSource={data1} />}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Orders
+export default Orders;
