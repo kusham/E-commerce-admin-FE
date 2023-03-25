796b7e9630c962bf6247ae0dab0ef108640a8ba8
kusham <kusham1998@gmail.com>
2023-03-26 03:09:54 +0530

    getUserFromLocalStorage action

--- src/action/auth/index.jsx
+++ src/action/auth/index.jsx
@@ -1,35 +1,47 @@
-import {createAsyncThunk } from "@reduxjs/toolkit";
-import {getOrder, login} from '../../api/auth'
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { getOrder, login } from "../../api/auth";
 
 export const login = createAsyncThunk(
-    "auth/login",
-    async (userData, thunkAPI) => {
-      try {
-        return await login(userData);
-      } catch (error) {
-        return thunkAPI.rejectWithValue(error);
-      }
+  "auth/login",
+  async (userData, thunkAPI) => {
+    try {
+      return await login(userData);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
-  );
+  }
+);
 
-  export const getOrderByUser = createAsyncThunk(
-    "order/get-order",
-    async (id, thunkAPI) => {
-      try {
-        return await getOrder(id);
-      } catch (error) {
-        return thunkAPI.rejectWithValue(error);
-      }
+export const getOrderByUser = createAsyncThunk(
+  "order/get-order",
+  async (id, thunkAPI) => {
+    try {
+      return await getOrder(id);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
-  );
+  }
+);
 
-  export const getOrders = createAsyncThunk(
-    "order/get-orders",
-    async (thunkAPI) => {
-      try {
-        return await authService.getOrders();
-      } catch (error) {
-        return thunkAPI.rejectWithValue(error);
-      }
+export const getOrders = createAsyncThunk(
+  "order/get-orders",
+  async (thunkAPI) => {
+    try {
+      return await authService.getOrders();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
-  );
+  }
+);
+
+const getUserFromLocalStorage = localStorage.getItem("user")
+  ? JSON.parse(localStorage.getItem("user"))
+  : null;
+const initialState = {
+  user: getUserFromLocalStorage,
+  orders: [],
+  isError: false,
+  isLoading: false,
+  isSuccess: false,
+  message: "",
+};
