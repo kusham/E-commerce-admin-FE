5eb8732c240fe4a15a63f2855f2777a4d6a1755d
kusham <kusham1998@gmail.com>
2023-03-27 03:14:12 +0530

    move files

---
+++ src/utils/TableColumns/CustomerColumns.js
@@ -0,0 +1,19 @@
+export const columns = [
+    {
+      title: "SNo",
+      dataIndex: "key",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      sorter: (a, b) => a.name.length - b.name.length,
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+    },
+    {
+      title: "Mobile",
+      dataIndex: "mobile",
+    },
+  ];
