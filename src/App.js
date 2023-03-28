8842b86432065d730bb515b848c87e94d30b5a45
kusham <kusham1998@gmail.com>
2023-03-29 03:23:49 +0530

    ResetPassword route add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
+import ResetPassword from "./pages/ResetPassword";
 
 function App() {
   return (
     <Router>
       <Route path="/" element={<Login />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+
     </Router>
   );
 }
