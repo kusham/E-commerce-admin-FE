0436a7b986efc940ddfe13563a42b5626fa7b135
kusham <kusham1998@gmail.com>
2023-03-30 03:41:12 +0530

    render product details

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
-import { columns } from '../utils/TableColumns/ProductListColumns'
+import React, { useEffect } from "react";
+import { Table } from "antd";
+import { BiEdit } from "react-icons/bi";
+import { AiFillDelete } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { columns } from "../utils/TableColumns/ProductListColumns";
+import { getProducts } from "../action/product";
 
 const ProductList = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getProducts());
+  }, []);
+  const productState = useSelector((state) => state.product.products);
+  const data = [];
+  for (let i = 0; i < productState.length; i++) {
+    data.push({
+      key: i + 1,
+      title: productState[i].title,
+      brand: productState[i].brand,
+      category: productState[i].category,
+      color: productState[i].color,
+      price: `${productState[i].price}`,
+      action: (
+        <>
+          <Link to="/" className=" fs-3 text-danger">
+            <BiEdit />
+          </Link>
+          <Link className="ms-3 fs-3 text-danger" to="/">
+            <AiFillDelete />
+          </Link>
+        </>
+      ),
+    });
+  }
   return (
     <div>
       <h3 className="mb-4 title">Products</h3>
       <div>
-        <Table columns={columns} dataSource={} />
+        <Table columns={columns} dataSource={data} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductList
+export default ProductList;
