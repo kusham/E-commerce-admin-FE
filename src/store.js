6ce081edc9f7a0b87f8adee2fbdd8de9c38e462a
kusham <kusham1998@gmail.com>
2023-03-30 03:39:55 +0530

    add customerSlice

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./reducers/authReducers";
+import { customerSlice } from "./reducers/customerReducers";
 export const store = configureStore({
   reducer: {
     auth: authSlice,
+    customer: customerSlice,
   },
 });
