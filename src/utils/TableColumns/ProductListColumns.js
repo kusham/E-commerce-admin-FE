9f035d94b3d2d17832c3e564ef3527e144ced9dc
kusham <kusham1998@gmail.com>
2023-03-30 03:29:22 +0530

    ProductList column data

---
+++ src/utils/TableColumns/ProductListColumns.js
@@ -0,0 +1,34 @@
+export const columns = [
+    {
+      title: "SNo",
+      dataIndex: "key",
+    },
+    {
+      title: "Title",
+      dataIndex: "title",
+      sorter: (a, b) => a.title.length - b.title.length,
+    },
+    {
+      title: "Brand",
+      dataIndex: "brand",
+      sorter: (a, b) => a.brand.length - b.brand.length,
+    },
+    {
+      title: "Category",
+      dataIndex: "category",
+      sorter: (a, b) => a.category.length - b.category.length,
+    },
+    {
+      title: "Color",
+      dataIndex: "color",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      sorter: (a, b) => a.price - b.price,
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+    },
+  ];
