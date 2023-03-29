7a246ead2a45e37df0f088ccc7ca75aa63f34bbb
kusham <kusham1998@gmail.com>
2023-03-30 03:40:07 +0530

    create customer reducer

---
+++ src/reducers/customerReducers.jsx
@@ -0,0 +1,30 @@
+const initialState = {
+    customers: [],
+    isError: false,
+    isLoading: false,
+    isSuccess: false,
+    message: "",
+  };
+  export const customerSlice = createSlice({
+    name: "users",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+      builder
+        .addCase(getUsers.pending, (state) => {
+          state.isLoading = true;
+        })
+        .addCase(getUsers.fulfilled, (state, action) => {
+          state.isLoading = false;
+          state.isError = false;
+          state.isSuccess = true;
+          state.customers = action.payload;
+        })
+        .addCase(getUsers.rejected, (state, action) => {
+          state.isLoading = false;
+          state.isError = true;
+          state.isSuccess = false;
+          state.message = action.error;
+        });
+    },
+  });
