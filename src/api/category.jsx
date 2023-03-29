37b5b1ce0a8f80d4934ebf975a5e5f434d791a52
kusham <kusham1998@gmail.com>
2023-03-30 03:50:30 +0530

    createCategory api

--- src/api/category.jsx
+++ src/api/category.jsx
@@ -1,8 +1,15 @@
 import axios from "axios";
 import { base_url } from "../utils/baseUrl";
+import { config } from "../utils/axiosConfig";
 
 const getProductCategories = async () => {
   const response = await axios.get(`${base_url}category/`);
 
   return response.data;
 };
+
+const createCategory = async (category) => {
+    const response = await axios.post(`${base_url}category/`, category, config);
+  
+    return response.data;
+  };
