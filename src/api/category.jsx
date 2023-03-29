67cab9264686aee94c7ee23c7ecb3bff061c7743
kusham <kusham1998@gmail.com>
2023-03-30 03:49:19 +0530

    getProductCategories api

---
+++ src/api/category.jsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { base_url } from "../utils/baseUrl";
+
+const getProductCategories = async () => {
+  const response = await axios.get(`${base_url}category/`);
+
+  return response.data;
+};
