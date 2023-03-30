4886d2ff3ff31ff3cae7c0d1218a6844ac17de65
kusham <kusham1998@gmail.com>
2023-03-31 04:10:10 +0530

    createBrand api

--- src/api/brand.jsx
+++ src/api/brand.jsx
@@ -1,8 +1,14 @@
 import axios from "axios";
 import { base_url } from "../utils/baseUrl";
+import { config } from "../utils/axiosConfig";
 
 export const getBrands = async () => {
   const response = await axios.get(`${base_url}brand/`);
 
   return response.data;
 };
+export const createBrand = async (brand) => {
+    const response = await axios.post(`${base_url}brand/`, brand, config);
+  
+    return response.data;
+  };
