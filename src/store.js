f3958a34a204a7089f50b281edfa0f8cd28767bd
kusham <kusham1998@gmail.com>
2023-03-31 04:07:40 +0530

    add brand reducer

--- src/store.js
+++ src/store.js
@@ -2,13 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./reducers/authReducers";
 import { customerSlice } from "./reducers/customerReducers";
 import { productSlice } from "./reducers/productReducers";
-import { categorySlice } from "./reducers/categoryReducers";
+import { categorySlice } from "./reducers/productCategoryReducers";
+import { brandSlice } from "./reducers/brandReducers";
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     customer: customerSlice,
     product: productSlice,
     pCategory: categorySlice,
+    brand: brandSlice,
 
   },
 });
