860a74dff820204bdbda4ac9b2a34c2b4243ca9f
kusham <kusham1998@gmail.com>
2023-03-31 04:11:18 +0530

    format

--- src/api/brand.jsx
+++ src/api/brand.jsx
@@ -8,28 +8,28 @@ export const getBrands = async () => {
   return response.data;
 };
 export const createBrand = async (brand) => {
-    const response = await axios.post(`${base_url}brand/`, brand, config);
-  
-    return response.data;
-  };
+  const response = await axios.post(`${base_url}brand/`, brand, config);
 
-  export const updateBrand = async (brand) => {
-    const response = await axios.put(
-      `${base_url}brand/${brand.id}`,
-      { title: brand.brandData.title },
-      config
-    );
-  
-    return response.data;
-  };
-  export const getBrand = async (id) => {
-    const response = await axios.get(`${base_url}brand/${id}`, config);
-  
-    return response.data;
-  };
+  return response.data;
+};
 
-  export const deleteBrand = async (id) => {
-    const response = await axios.delete(`${base_url}brand/${id}`, config);
-  
-    return response.data;
-  };
+export const updateBrand = async (brand) => {
+  const response = await axios.put(
+    `${base_url}brand/${brand.id}`,
+    { title: brand.brandData.title },
+    config
+  );
+
+  return response.data;
+};
+export const getBrand = async (id) => {
+  const response = await axios.get(`${base_url}brand/${id}`, config);
+
+  return response.data;
+};
+
+export const deleteBrand = async (id) => {
+  const response = await axios.delete(`${base_url}brand/${id}`, config);
+
+  return response.data;
+};
