619e010bb7ec224dcad77df39d816c572aad4c5b
kusham <kusham1998@gmail.com>
2023-03-31 04:09:04 +0530

    brand list column data

---
+++ src/utils/TableColumns/BrandListColumns.js
@@ -0,0 +1,15 @@
+const columns = [
+    {
+      title: "SNo",
+      dataIndex: "key",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      sorter: (a, b) => a.name.length - b.name.length,
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+    },
+  ];
