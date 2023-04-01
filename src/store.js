66995ff8f115a0a6514f4c44d7a41a376e637b8f
kusham <kusham1998@gmail.com>
2023-04-02 03:32:00 +0530

    add reducers

--- src/store.js
+++ src/store.js
@@ -4,6 +4,8 @@ import { customerSlice } from "./reducers/customerReducers";
 import { productSlice } from "./reducers/productReducers";
 import { categorySlice } from "./reducers/productCategoryReducers";
 import { brandSlice } from "./reducers/brandReducers";
+import { colorSlice } from "./reducers/ColorReducers";
+import { uploadSlice } from "./reducers/uploadReducer";
 export const store = configureStore({
   reducer: {
     auth: authSlice,
@@ -11,6 +13,8 @@ export const store = configureStore({
     product: productSlice,
     pCategory: categorySlice,
     brand: brandSlice,
+    color: colorSlice,
+    upload: uploadSlice,
 
   },
 });
