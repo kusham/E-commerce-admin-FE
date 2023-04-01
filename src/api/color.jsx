81cb11cd25e45fad1257c4f7c5233160bc5c33a5
kusham <kusham1998@gmail.com>
2023-04-02 03:38:42 +0530

    createColor api

--- src/api/color.jsx
+++ src/api/color.jsx
@@ -1,8 +1,15 @@
 import axios from "axios";
 import { base_url } from "../utils/baseUrl";
+import { config } from "../utils/axiosConfig";
 
-const getColors = async () => {
+export const getColors = async () => {
   const response = await axios.get(`${base_url}color/`);
+
+  return response.data;
+};
+
+export const createColor = async (color) => {
+  const response = await axios.post(`${base_url}color/`, color, config);
 
   return response.data;
 };
