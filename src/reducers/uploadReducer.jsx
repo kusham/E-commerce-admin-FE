3a1f40c1a13afad2afae8c89ba617533c2cdcefd
kusham <kusham1998@gmail.com>
2023-04-02 03:32:31 +0530

    upload reducer

---
+++ src/reducers/uploadReducer.jsx
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const uploadSlice = createSlice({
+  name: "imaegs",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(uploadImg.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(uploadImg.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.images = action.payload;
+      })
+      .addCase(uploadImg.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(delImg.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(delImg.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.images = [];
+      })
+      .addCase(delImg.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.payload;
+      });
+  },
+});
