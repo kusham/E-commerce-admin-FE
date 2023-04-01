fb2d28ce5938bc6014df06d7a5b7637328965f82
kusham <kusham1998@gmail.com>
2023-04-02 03:42:34 +0530

    createColor action

--- src/action/color/index.jsx
+++ src/action/color/index.jsx
@@ -1,11 +1,21 @@
-import {  createAsyncThunk } from "@reduxjs/toolkit";
-import { getColors } from "../../api/color";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createColor, getColors } from "../../api/color";
 
 export const getColors = createAsyncThunk(
   "color/get-colors",
   async (thunkAPI) => {
     try {
       return await getColors();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+export const createColor = createAsyncThunk(
+  "color/create-color",
+  async (colorData, thunkAPI) => {
+    try {
+      return await createColor(colorData);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
