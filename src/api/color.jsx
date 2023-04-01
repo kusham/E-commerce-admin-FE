f755c13523ca16158e9c1d3e6bbfe1df55b45a8e
kusham <kusham1998@gmail.com>
2023-04-02 03:38:00 +0530

    getColors api

---
+++ src/api/color.jsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { base_url } from "../utils/baseUrl";
+
+const getColors = async () => {
+  const response = await axios.get(`${base_url}color/`);
+
+  return response.data;
+};
